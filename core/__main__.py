18c3f75c9c364d3244d06116a8e66f9b023ca95d
offthetab <pavelgrom58@gmail.com>
2024-07-15 02:27:27 +0300

    added DB middleware to main

--- core/__main__.py
+++ core/__main__.py
@@ -15,6 +15,8 @@ from core.config import settings
 
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
+from core.middlewares.db import AsyncSeesionMiddleware
+
 
 async def on_startup(bot: Bot):
     await bot.send_message(chat_id=settings.admin, text="Бот был успешно запущен")
@@ -23,11 +25,13 @@ async def on_startup(bot: Bot):
 # Запуск бота
 async def main():
     engine = create_async_engine(url=settings.db_url, echo=True)
-    sessionmaker = async_sessionmaker(engine, expire_on_commit=False)
+    async_session = async_sessionmaker(engine, expire_on_commit=False)
 
     bot = Bot(token=settings.bot_token.get_secret_value(), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher()
     dp.startup.register(on_startup)
+
+    dp.update.middleware(AsyncSeesionMiddleware(async_session))
 
     dp.include_routers(admin_router, member_router, left_router)
 
